854f73b3bcb5be69ac0e320fec0ab3a0403e7c0c
rssnsj <rssnsj@gmail.com>
2015-02-05 14:38:17 +0800

    Enhanced scripts.

--- scripts/hc5x61-repack-ss.sh
+++ scripts/hc5x61-repack-ss.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-[ -z "$U" ] && U="http://v.rssn.cn/ralink/packages/"
+get_ss_firmware_name()
+{
+	local orig_name="$1"
+	echo `echo "$orig_name" | sed 's/\.bin$//'`-Shadowsocks.bin
+}
 
+# --------------------------------------------------
+
+[ -z "$U" ] && U="http://v.rssn.cn/ralink/packages/"
 case "$U" in
 	*/) : ;;
 	*) U="$U/" ;;
 esac
 
 M=""
+F=""
 case "$1" in
-	HC5?6?) :;;
-	HC5?6?-*) M=`echo "$1" | awk -F- '{print $1}'`;;
+	HC5?6?) M="$1"; shift 1; F="$1"; shift 1;;
+	HC5?6?-*) F="$1"; M=`echo "$F" | awk -F- '{print $1}'`; shift 1;;
 	*) echo "*** Unable to determine model type of this firmware. Please specify model name in arguments before file name."; exit 1;;
 esac
 
@@ -20,8 +28,8 @@ opkg install '${U}autossh_1.4b-20140307.1_ralink.ipk'
 opkg install '${U}dnsmasq-salist_2.71-1_ralink.ipk'
 opkg install '${U}openssh-redir-client_6.1p1-20150202_ralink.ipk'
 opkg install '${U}vanillass-libev_1.6.2_ralink.ipk'
-opkg install '${U}shadowsocks-tools_1-20150108_ralink.ipk' --force-overwrite
-" $M "$@"
+opkg install '${U}shadowsocks-tools_1-20150108_ralink.ipk' --force-overwrite --force-depends
+" $M "$F" -o `get_ss_firmware_name "$F"` "$@"
 
 exit 0
 
--- scripts/hc6361-repack-ss.sh
+++ scripts/hc6361-repack-ss.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-[ -z "$U" ] && U="http://v.rssn.cn/HC6361/packages/"
+get_ss_firmware_name()
+{
+	local orig_name="$1"
+	echo `echo "$orig_name" | sed 's/\.bin$//'`-Shadowsocks.bin
+}
 
+# --------------------------------------------------
+
+[ -z "$U" ] && U="http://v.rssn.cn/HC6361/packages/"
 case "$U" in
 	*/) : ;;
 	*) U="$U/" ;;
 esac
 
+M=""
+F=""
 case "$1" in
-	HC6361) shift 1:;;
+	HC6361) M="$1"; shift 1; F="$1"; shift 1;;
+	HC6361-*) F="$1"; M=`echo "$F" | awk -F- '{print $1}'`; shift 1;;
+	*) echo "*** Unable to determine model type of this firmware. Please specify model name in arguments before file name."; exit 1;;
 esac
 
 hiwifi-repack.sh -e -x "
@@ -18,7 +29,7 @@ opkg install '${U}dnsmasq-salist_2.71-1_ar71xx.ipk'
 opkg install '${U}openssh-redir-client_6.1p1-20150202_ar71xx.ipk'
 opkg install '${U}vanillass-libev_1.6.2_ar71xx.ipk'
 opkg install '${U}shadowsocks-tools_1-20150108_ar71xx.ipk' --force-overwrite --force-depends
-" HC6361 "$@"
+" $M "$F" -o `get_ss_firmware_name "$F"` "$@"
 
 exit 0
 
