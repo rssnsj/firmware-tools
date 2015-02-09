258ed1e11081d9d106f56ff43394493115de549f
rssnsj <rssnsj@gmail.com>
2015-02-09 23:33:24 +0800

    tl-ar71xx-repack.sh: Modified for all ar71xx-based firmware.

---
+++ scripts/tl-ar71xx-repack.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+openwrt-repack.sh -w -x '
+set -e
+opkg update
+opkg install kmod-usb-storage kmod-fs-ext4 kmod-fs-vfat kmod-sit kmod-ipip kmod-gre kmod-nls-cp437 kmod-nls-iso8859-1
+opkg install libevent2 pptpd xl2tpd ntfs-3g ntfs-3g-utils luci-app-samba luci-proto-ipv6 openvpn-openssl ipset
+opkg install tcpdump iftop iperf-mt
+' "$@"
+
+#
+# http://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/openwrt-ar71xx-generic-tl-mr11u-v2-squashfs-sysupgrade.bin
+#
